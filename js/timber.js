be564d23031ffac5c873062ab5cf0a94121b853d
John Fish <john@johnafish.ca>
2015-10-24 17:56:41 -0400

    Realtime updating from Firebase

--- js/timber.js
+++ js/timber.js
@@ -1,2 +1,28 @@
-var fireBaseReference = new Firebase("https://treetup.firebaseio.com/");
-var geoFire = new GeoFire(reference.child("_geofire"));
+var fireBaseReference = new Firebase("https://treetup.firebaseio.com");
+var treeReference = fireBaseReference.child("trees");
+var geoFire = new GeoFire(fireBaseReference.child("_geofire"));
+
+var treesInQuery = new Array();
+var latitude = -80.5256895;
+var longitude = 43.4633228;
+var numtrees = 0;
+
+var queryRadius = 0.5;
+
+var watchPosition = navigator.geolocation.getCurrentPosition(function(position) {
+  latitude = position.coords.longitude;
+  longitude = position.coords.latitude;
+  console.log(latitude+","+longitude);
+});
+
+var treeQuery = geoFire.query({
+    center: [latitude,longitude],
+    radius: queryRadius
+});
+
+treeQuery.on("key_entered", function(key,location,distance){
+    treesInQuery.push(key);
+    treeReference.child(treesInQuery[treesInQuery.length-1]).on("value", function(snapshot) {
+      console.log(snapshot.val());
+    });
+});
