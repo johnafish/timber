00d41e52707e8830cd0b9f90ba55033d5e748e26
John Fish <john@johnafish.ca>
2015-10-24 22:59:10 -0400

    Centers around person and shows nearby trees as dots

--- js/timber.js
+++ js/timber.js
@@ -7,47 +7,63 @@ var latitude = -80.5256895;
 var longitude = 43.4633228;
 var numtrees = 0;
 
-var queryRadius = 0.5;
-
-var watchPosition = navigator.geolocation.getCurrentPosition(function(position) {
+var queryRadius = 0.3;
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+var watchPosition = navigator.geolocation.watchPosition(function(position) {
   latitude = position.coords.longitude;
   longitude = position.coords.latitude;
-  console.log(latitude+","+longitude);
-});
+  var treeQuery = geoFire.query({
+      center: [latitude,longitude],
+      radius: queryRadius
+  });
+  var person = {lat: longitude, lng: latitude}
+  createPerson(person);
+  map.setCenter(person);
 
-var treeQuery = geoFire.query({
-    center: [latitude,longitude],
-    radius: queryRadius
-});
+  treeQuery.on("key_entered", function(key,location,distance){
+  	if (treesInQuery.indexOf(key) === -1) {
+  	    treesInQuery.push(key);
+  	}
+
+      treeReference.child(key).on("value", function(snapshot) {
+      	tree = snapshot.val();
+      	fireBaseReference.child("_geofire").child(key).on("value", function(snap) {
+      		treeGeo = snap.val();
+      		treeLong = treeGeo.l[0];
+      		treeLat = treeGeo.l[1];
+      		createTree(treeGeo);
+      	})
+      })
+  });
+}, function error(err){
+    console.log("error");
+}, options);
+
+
+
 
 var trees = {};
 var treeGeo, tree, treeLong, treeLat;
 
-//cannot change the global variables trees and stuff inside the functions?????
-
-function setTrees(newTrees) {
-	trees = newTrees;
-	console.log('trees set to ' + newTrees);
+function createPerson(position) {
+    var marker = new google.maps.Marker({
+    //   icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/2000px-Person_icon_BLACK-01.svg.png",
+      position: new google.maps.LatLng(position.lat, position.lng),
+      optimized: true,
+      map: map
+    });
+    return marker;
 }
 
-	
-treeQuery.on("key_entered", function(key,location,distance){
-
-	if (treesInQuery.indexOf(key) === -1) {
-	    treesInQuery.push(key);
-	}
-
-    treeReference.child(key).on("value", function(snapshot) {
-    	tree = snapshot.val();
-    	fireBaseReference.child("_geofire").child(key).on("value", function(snap) {
-    		treeGeo = snap.val();
-    		treeLong = treeGeo.l[0];
-    		treeLat = treeGeo.l[1];
-
-    		trees[key] = [treeLong, treeLat];
-    		//console.log(trees);
-    		setTrees(trees);
-    	})
-    })
-    console.log(trees);
-});
+function createTree(tree) {
+  var marker = new google.maps.Marker({
+    icon: "https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle_blue.png",
+    position: new google.maps.LatLng(tree.l[1], tree.l[0]),
+    optimized: true,
+    map: map
+  });
+}
