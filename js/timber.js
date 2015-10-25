13255427bb757b2ea9a7d22fdf4bab1c2c6a72d6
Alex Foley <foleyfactor98@gmail.com>
2015-10-25 09:16:26 -0400

    Removed unnecessary files, map updates.

--- js/timber.js
+++ js/timber.js
@@ -14,7 +14,7 @@ var options = {
   maximumAge: 0
 };
 
-var markers = new Array();
+var markers = {};
 
 var watchPosition = navigator.geolocation.watchPosition(function(position) {
   latitude = position.coords.longitude;
@@ -30,29 +30,25 @@ var watchPosition = navigator.geolocation.watchPosition(function(position) {
 
   treeQuery.on("key_entered", function(key,location,distance){
       console.log(distance);
-  	if (treesInQuery.indexOf(key) === -1) {
-  	    treesInQuery.push(key);
-  	}
 
       treeReference.child(key).on("value", function(snapshot) {
       	tree = snapshot.val();
       	fireBaseReference.child("_geofire").child(key).on("value", function(snap) {
       		treeGeo = snap.val();
-      		treeLong = treeGeo.l[0];
-      		treeLat = treeGeo.l[1];
-      		createTree(treeGeo);
+      		createTree(treeGeo, key);
       	})
       })
   });
+
+  treeQuery.on("key_exited", function(key,location,distance){
+    removeTree(key);
+  });
+
 }, function error(err){
     console.log("error");
 }, options);
 
-
-
-
-var trees = {};
-var treeGeo, tree, treeLong, treeLat;
+var treeGeo;
 
 function createPerson(position) {
     var marker = new google.maps.Marker({
@@ -61,23 +57,72 @@ function createPerson(position) {
       optimized: true,
       map: map
     });
-  markers.push(marker);
+  markers['person'] = marker;
 
 }
 
-function createTree(tree) {
-  var marker = new google.maps.Marker({
-    icon: "https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle_blue.png",
-    position: new google.maps.LatLng(tree.l[1], tree.l[0]),
-    optimized: true,
-    map: map
-  });
-  markers.push(marker);
+function createTree(tree, treeID) {
+	var marker = new google.maps.Marker({
+	icon: "https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle_blue.png",
+	position: new google.maps.LatLng(tree.l[1], tree.l[0]),
+	optimized: true,
+	map: map
+	});
+	markers[treeID] = marker;
+	marker.addListener('click', function(){
+  		//createLink(treeID, new Date());
+  		vote(1, treeID);
+  })
+}
+
+function removeTree(treeID) {
+	markers[treeID].setMap(null);
 }
 
 function removeMarkers() {
-    for (var i = 0; i < markers.length; i++) {
+    for (var i in markers) {
         markers[i].setMap(null);
     }
-    markers = new Array();
+}
+
+var map;
+function initMap() {
+	if (navigator.geolocation) {
+		
+		navigator.geolocation.getCurrentPosition(function(position) {
+			latitude = position.coords.latitude;
+			longitude = position.coords.longitude;
+
+		});
+
+		zoomAmount = 17;
+
+	} else {
+
+		latitude = 43.477480;
+		longitude = -80.544317;
+		zoomAmount = 11;
+	}
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: new google.maps.LatLng(longitude, latitude),
+		scrollwheel: false,
+		zoom: zoomAmount
+	});
+
+}
+
+function createLink(treeID, d) {
+	meetingTime = d.getTime();
+	console.log('http://timberapp.ml/treetup.php?time='+Math.floor((meetingTime/1000))+'&tree='+treeID);
+}
+
+function vote(delta, treeID) {
+	var currScore
+	treeReference.child(treeID.toString()).child('score').on('value', function(snapshot){
+		currScore = snapshot.val();
+		console.log(currScore);
+	})
+	currScore += delta;
+	treeReference.child(treeID.toString()).child('score').set(currScore);
 }
