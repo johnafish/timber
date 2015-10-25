7bb3d5b526a0a80ce9bee88ba7ef66aae704790c
John Fish <john@johnafish.ca>
2015-10-24 22:20:06 -0400

    :hamburger: Website now works with directions
    http://timberapp.ml/treetup.php?time=1445753201&tree=10564 :dancer:

--- website/treetup.php
+++ website/treetup.php
@@ -11,13 +11,22 @@ $time = $_GET["time"];
 </head>
 <body>
 <header>
-    <h1>LETS <span>TREE</span>TUP</h1>
+    <h1>LET'S <span>TREE</span>TUP</h1>
 </header>
     <h2 id="when"></h2>
     <div id="map"></div>
+<script src="https://cdn.firebase.com/js/client/2.2.5/firebase.js"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDSvB66Bvb1NwyMblo_HtvhgIzbY_T9_Q4&callback=initMap"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
 <script>
 var tree = "<?php echo $treeID; ?>";
 var time = parseInt("<?php echo $time; ?>");
+var fireBaseReference = new Firebase("https://treetup.firebaseio.com");
+var geoFire = fireBaseReference.child("_geofire");
+var treeLatLong = {lat:43.4633228 , lng: -80.5256895};
+
+
 var d = new Date(0);
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 d.setUTCSeconds(time);
@@ -42,6 +51,61 @@ if (minutes<10){
 }
 var dateString = "at "+hours+":"+minutes+amPm+" on "+months[d.getMonth()]+" "+d.getDate();
 document.getElementById("when").innerHTML = dateString;
+var latitude,longitude;
+var userPosition;
+function setPosition() {
+    if (navigator.geolocation){
+        var location = navigator.geolocation.getCurrentPosition(function(position) {
+          setLatLong(position.coords.latitude,position.coords.longitude);
+        });
+        initMap();
+    }
+}
+
+
+function initMap(){
+    var place = {lat:43.4617098 , lng: -80.5097937};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: place,
+        scrollwheel: false,
+        zoom: 18
+    });
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var you = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+            map.setCenter(you);
+            var directionsDisplay = new google.maps.DirectionsRenderer({
+                map: map
+            });
+            geoFire.child(tree).on("value", function(snapshot) {
+            	var tree = snapshot.val();
+                treeLatLong = {lat: tree.l[1], lng: tree.l[0]};
+                var request = {
+                    destination: treeLatLong,
+                    origin: you,
+                    travelMode: google.maps.TravelMode.DRIVING
+                };
+
+                var directionsService = new google.maps.DirectionsService();
+                directionsService.route(request, function(response, status) {
+                    if (status == google.maps.DirectionsStatus.OK) {
+                            directionsDisplay.setDirections(response);
+                    }
+                });
+            });
+            });
+
+    }
+
+
+}
+
+
 </script>
 </body>
 </html>
